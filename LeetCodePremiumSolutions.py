0e31784239537b79147bbaf5bc5f10af2b616571
susmita-dutta-cs <dutta.or.susmita@gmail.com>
2021-04-26 15:28:43 +0200

    update

--- LeetCodePremiumSolutions.py
+++ LeetCodePremiumSolutions.py
@@ -4,6 +4,7 @@
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+
         current_subarray = max_subarray = nums[0]
         for num in nums[1:]:
             current_subarray = max(num, current_subarray + num)
