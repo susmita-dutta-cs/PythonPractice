54dbfcb60d8cd5ffc3b1d3f97c800a995b25b292
susmita-dutta-cs <dutta.or.susmita@gmail.com>
2021-04-29 14:52:32 +0200

    combining two sorted list

--- LeetCodePremiumSolutions.py
+++ LeetCodePremiumSolutions.py
@@ -2,11 +2,46 @@
 # largest sum and return its sum.
 #kadanes algorithm - dynamic programming
 
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
 
-        current_subarray = max_subarray = nums[0]
-        for num in nums[1:]:
-            current_subarray = max(num, current_subarray + num)
-            max_subarray = max(max_subarray, current_subarray)
-        return max_subarray
+# def maxSubArray(self, nums: List[int]) -> int:
+#
+#         current_subarray = max_subarray = nums[0]
+#         for num in nums[1:]:
+#             current_subarray = max(num, current_subarray + num)
+#             max_subarray = max(max_subarray, current_subarray)
+#         return max_subarray
+
+
+# Python3 code to demonstrate
+# to combine two sorted list
+# using naive method
+
+# initializing lists
+test_list1 = [1, 5, 6, 9, 11]
+test_list2 = [3, 4, 7, 8, 10]
+
+# printing original lists
+print ("The original list 1 is : " + str(test_list1))
+print ("The original list 2 is : " + str(test_list2))
+
+# using naive method
+# to combine two sorted lists
+size_1 = len(test_list1)
+size_2 = len(test_list2)
+
+res = []
+i, j = 0, 0
+
+while i < size_1 and j < size_2:
+    if test_list1[i] < test_list2[j]:
+     res.append(test_list1[i])
+     i += 1
+
+    else:
+        res.append(test_list2[j])
+        j += 1
+
+res = res + test_list1[i:] + test_list2[j:]
+
+# printing result
+print ("The combined sorted list is : " + str(res))
