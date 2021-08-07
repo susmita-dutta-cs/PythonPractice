7f7bc420e19332ce38178887e42fd8e19740d365
susmita-dutta-cs <dutta.or.susmita@gmail.com>
2021-08-07 02:02:42 +0200

    valid parenthesis

---
+++ nestedlist.py
@@ -0,0 +1,10 @@
+# 2-D List
+matrix = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+
+flatten_matrix = []
+
+for sublist in matrix:
+    for val in sublist:
+        flatten_matrix.append(val)
+
+print(flatten_matrix)
