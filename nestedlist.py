977ec63d52460ae99819ff3de4312b1f529dd992
susmita-dutta-cs <dutta.or.susmita@gmail.com>
2021-08-07 02:07:17 +0200

    valid parenthesis

--- nestedlist.py
+++ nestedlist.py
@@ -8,3 +8,37 @@ for sublist in matrix:
         flatten_matrix.append(val)
 
 print(flatten_matrix)
+
+# 2-D List of planets
+planets = [['Mercury', 'Venus', 'Earth'], ['Mars', 'Jupiter', 'Saturn'], ['Uranus', 'Neptune', 'Pluto']]
+
+flatten_planets = []
+
+for sublist in planets:
+    for planet in sublist:
+
+        if len(planet) < 6:
+            flatten_planets.append(planet)
+
+print(flatten_planets)
+
+# Here we have used the technique of Bubble Sort to perform the sorting.
+# We have tried to access the second element of the sublists using the nested loops.
+# This performs the in-place method of sorting. The time complexity is
+# similar to the Bubble Sort i.e., O(n^2)
+# Python code to sort the lists using the second element of sublists
+# Inplace way to sort, use of third variable
+def Sort(sub_li):
+    l = len(sub_li)
+    for i in range(0, l):
+        for j in range(0, l - i - 1):
+            if (sub_li[j][1] > sub_li[j + 1][1]):
+                tempo = sub_li[j]
+                sub_li[j] = sub_li[j + 1]
+                sub_li[j + 1] = tempo
+    return sub_li
+
+
+# Driver Code
+sub_li = [['rishav', 10], ['akash', 5], ['ram', 20], ['gaurav', 15]]
+print(Sort(sub_li))
